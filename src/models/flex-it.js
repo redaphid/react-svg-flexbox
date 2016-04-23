4971077810128490fd05ec6a6ff8ac6670d6b53a
redaphid <iam@aaronherres.com>
2016-04-23 00:52:25 -0700

    less console.logs and other useless stuff

--- src/models/flex-it.js
+++ src/models/flex-it.js
@@ -17,21 +17,17 @@ export default function flexIt (node) {
   return applyStyleTree(styleTree)
 }
 
-function applyStyleTree(styleTree) {
-  if(!_.isObject(styleTree)) {
-    console.log(styleTree, 'is not an object')
-  }
+function applyStyleTree({dom, children, layout}) {
+  if(_.isEmpty(children)) return cloneElement(dom, layout)
 
-  const {dom, children, layout} = styleTree
-  if(_.isEmpty(children)){
-    return cloneElement(dom, layout)
-  }
-
-  return cloneElement(dom, layout, _.map(children, applyStyleTree))
+  return cloneElement(
+    dom,
+    _.extend({flexed: true}, layout),
+    _.map(children, applyStyleTree)
+  )
 }
 
 function getStyleTree(node) {
-  console.log('getStyleTree', node)
   return {
     dom: node,
     style: getStyle(node),
@@ -40,7 +36,6 @@ function getStyleTree(node) {
 }
 
 function getStyleChildren(children) {
-  console.log('getStyleChildren', {children})
   if( !_.isArray(children)) return []
 
   return _.compact( _.map(children, getStyleTree ) )
